96534d045038f9b0ce0c19b1c3e60f0ef28569ef
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-01-11 10:42:24 +0530

    cross field validation

--- angular/angconcepts/src/app/app.component.ts
+++ angular/angconcepts/src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent  {
   phoneNumber = 988889000;
   @Input('cityDetails') cityDetail;
   
-  streetBlurDetails() {
+  streetBlurDetails(val) {
 
   }
   updateCity(val){
--- angular/angconcepts/src/app/reactiveforms/reactiveforms.component.ts
+++ angular/angconcepts/src/app/reactiveforms/reactiveforms.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
+function phoneValidator(val:boolean){
+  if(val){
+    return null;
+  }
+  return {
+    match:true
+  }
+}
 @Component({
   selector: 'app-reactiveforms',
   templateUrl: './reactiveforms.component.html',
@@ -9,19 +17,29 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class ReactiveformsComponent implements OnInit {
   customersInformation:FormGroup;
   constructor() { }
+  
   customersFormDetails(){
     console.log(this.customersInformation);
   }
   ngOnInit() {
     this.customersInformation = new FormGroup({
-      firstName:new FormControl(''),
-      lastName: new FormControl(''),
-      email: new FormControl(''),
+      firstName:new FormControl('',[Validators.required,Validators.minLength(3)]),
+      lastName: new FormControl('',[Validators.required,Validators.minLength(3)]),
+      email: new FormControl('',[Validators.email]),
       phone: new FormControl(''),
       verify: new FormControl(true),
       rating: new FormControl(),
       notification: new FormControl('')
     })
+  }
+  setNotification(notify:string){
+    const phone = this.customersInformation.get('phone');
+    if(notify === 'text'){
+      phone.setValidators([Validators.required,Validators.minLength(10),Validators.maxLength(10)]);
+    }else{
+      phone.clearValidators();
+    }
+    phone.updateValueAndValidity();
   }
 
 }
