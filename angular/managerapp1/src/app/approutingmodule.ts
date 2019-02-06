6c88026e39d2e3af314a04998f72ec2f7a3f2b6c
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-02-06 10:18:37 +0530

    customer component

--- angular/managerapp1/src/app/approutingmodule.ts
+++ angular/managerapp1/src/app/approutingmodule.ts
@@ -1,6 +1,8 @@
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { NgModule } from '@angular/core';
 const route:Routes =[
+    {path:'', redirectTo:'/customers',pathMatch:'full'},
+    {path:'customers/:id',loadChildren :'./customer/customer.module#CustomerModule'},
     {path:'customers',loadChildren :'./customers/customers.module#CustomersModule'},
     {path:'orders',loadChildren:'./orders/orders.module#OrdersModule'},
 ]
