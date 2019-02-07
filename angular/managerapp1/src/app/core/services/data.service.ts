e8dd21421dc39c823ff94549321839fc2576e997
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-02-07 11:04:39 +0530

    loading data in customer details

--- angular/managerapp1/src/app/core/services/data.service.ts
+++ angular/managerapp1/src/app/core/services/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { CustomerI } from '../models/customersTypeDef';
 @Injectable()
 export class DataService {
 
@@ -8,11 +10,15 @@ export class DataService {
   getSampleData(){
     return   ['krishna','arjuna']
   }
-  getCustomersData(){
+  getCustomersData():Observable<any>{
     return this.http.get('/api/customers').pipe(
       map(res => res)
     )
   }
-  
+  getCustomerData(id:number):Observable<any>{
+    return this.http.get(`/api/customers/${id}`).pipe(
+      map(res => res)
+    )
+  }
 
 }
